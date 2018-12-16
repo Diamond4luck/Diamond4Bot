fe4db26b06870117b3057fcca4e3feb759fcc57f
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-12-16 20:40:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -173,7 +173,7 @@ async def rps(ctx):
 
 @client.command(pass_context=True)
 async def help(ctx):
-    anotherembed = discord.Embed(title="Everything you need here!")
+    aembed = discord.Embed(title="Everything you need here!")
     embed.add_field(name="!!power", value="Do !!power to see what hidden powers you have!")
     embed.add_field(name="!!logs", value="Do !!logs to see what recent changes I did to the bot!")
     embed.add_field(name="!!help", value="Do !!help to get help about the bot!")
@@ -187,7 +187,7 @@ async def help(ctx):
     embed.add_field(name="!!diary", value="Do !!diary and see other people's diaries!")
     help1 = await client.say(embed=embed)
     await client.say("Type help1/help2 to see the pages.")   
-    anotherembed2 = discord.Embed(title="Second page!")
+    embed2 = discord.Embed(title="Second page!")
     embed.add_field(name="!!game", value="Do !!game and the bot guesses your favourite game")
     embed.add_field(name="!!moti", value="Do !!diary and see a motivational message!")
     embed.add_field(name="!!love", value="Do !!love and see who loves who for how long!")
